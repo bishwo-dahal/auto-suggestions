1240dd52ec089727745ccb43014ce887dcd9e0f3
Bishwo Dahal <bishwodahal123@gmail.com>
2021-07-07 18:45:38 +0545

    created working suggestions

---
+++ auto.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <deque>
+#include <cctype>
+using namespace std;
+
+void displayCharVector(std::vector<char> arr)
+{
+	for (char val : arr)
+	{
+		std::cout << val;
+	}
+	std::cout << std::endl;
+}
+bool compare(char a, char b)
+{
+	if (toupper(a) == toupper(b))
+	{
+		return true;
+	}
+	return false;
+}
+void display(std::vector<std::pair<char, int>> val)
+{
+	for (auto curr : val)
+	{
+		std::cout << "Value = " << curr.first << " Index = " << curr.second << std::endl;
+	}
+}
+void displayVectorDeque(std::deque<std::vector<char>> val)
+{
+	for (std::vector<char> elem : val)
+	{
+		displayCharVector(elem);
+	}
+}
+int main()
+{
+	// (466551)
+	std::deque<std::vector<char>> words;
+	std::vector<std::pair<char, int>> wordsIndex;
+	fstream openFile("words.txt");
+	std::string word = "";
+	if (openFile)
+	{
+		int index = 0;
+		char beforeChar = ' ';
+		while (getline(openFile, word))
+		{
+			std::vector<char> stringVector(word.length());
+			std::copy(word.begin(), word.end(), stringVector.begin());
+			words.push_back(stringVector);
+			//pushing the vector to main deque
+			index = words.size();
+
+			if (!compare(stringVector.at(0), beforeChar))
+			{
+				wordsIndex.push_back(std::make_pair(tolower(stringVector.at(0)), index));
+			}
+			beforeChar = stringVector.at(0);
+		}
+	}
+	else
+	{
+		std::cout << "Could not find the dictionary " << std::endl;
+	}
+
+	std::string input;
+	std::cout << "Enter word to find ------------" << std::endl;
+	// std::cin >> input;
+	input = "helxo";
+	std::vector<char> inputVector(input.length());
+	std::copy(input.begin(), input.end(), inputVector.begin());
+	std::deque<vector<char>>::iterator foundIterator = std::find(words.begin(), words.end(), inputVector);
+
+	if (foundIterator == words.end())
+	{
+		//l
+		std::cout << "No matching words found " << std::endl;
+		std::cout << "Suggestions for you are \n";
+		char currentChar = inputVector.at(0);
+		std::vector<std::pair<char, int>>::iterator letterIterator = find_if(wordsIndex.begin(), wordsIndex.end(), [&currentChar](const std::pair<char, int> &element)
+																			 { return compare(element.first, currentChar); });
+		// just handling valid case only
+		if (letterIterator != wordsIndex.end())
+		{
+			int currentLetterIndex = (*letterIterator).second;
+			int nextLetterIndex = (*(letterIterator + 1)).second;
+			std::deque<std::vector<char>> suggestions(nextLetterIndex - currentLetterIndex);
+			// std::cout << "The size is " << suggestions.size();
+			std::deque<std::vector<char>>::iterator currentIterator = words.begin() + currentLetterIndex,
+													nextIterator = words.begin() + nextLetterIndex;
+			std::copy(currentIterator, nextIterator, suggestions.begin());
+
+			suggestions.erase(std::remove_if(
+								  suggestions.begin(), suggestions.end(),
+								  [&](const std::vector<char> elem)
+								  {
+									  return elem.size() != inputVector.size(); // put your condition here
+								  }),
+							  suggestions.end());
+
+			for (int cc = 1; cc < inputVector.size(); cc++)
+			{
+				std::deque<std::vector<char>> previousSuggestion = suggestions;
+
+				suggestions.erase(std::remove_if(suggestions.begin(), suggestions.end(),
+												 [&](const std::vector<char> elem)
+												 {
+													 return !compare(elem.at(cc), inputVector.at(cc));
+												 }),
+								  suggestions.end());
+
+				if (suggestions.size() == 0)
+				{
+					suggestions = previousSuggestion;
+					cc++;
+					if (cc < inputVector.size())
+					{
+						suggestions.erase(std::remove_if(suggestions.begin(), suggestions.end(),
+														 [&](const std::vector<char> elem)
+														 {
+															 //  std::cout << "elem =" << elem.at(cc) << "    input =" << inputVector.at(cc) << std::endl;
+															 return !compare(elem.at(cc), inputVector.at(cc));
+														 }),
+										  suggestions.end());
+						previousSuggestion = suggestions;
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+			displayVectorDeque(suggestions);
+		}
+	}
+	else
+	{
+		std::cout << "Found your word at the dictionary\n";
+		std::cout << "Here are the list of words near this one " << std::endl;
+		displayCharVector(*(foundIterator - 1));
+		displayCharVector(*(foundIterator + 1));
+	}
+
+	return 0;
+}
