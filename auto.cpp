2c51d4c426b0069157271158fb0f73ec320e8a48
Bishwo Dahal <bishwodahal123@gmail.com>
2021-07-07 20:20:42 +0545

    cleared unused code and much efficient

--- auto.cpp
+++ auto.cpp
@@ -42,27 +42,17 @@ int main()
 	// (466551)
 	std::deque<std::vector<char>> words;
 	//used  <std::vector<char>> because , it's much easier to go with it
-	std::vector<std::pair<char, int>> wordsIndex;
-	// It
+
 	fstream openFile("words.txt");
 	std::string word = "";
 	if (openFile)
 	{
-		int index = 0;
-		char beforeChar = ' ';
 		while (getline(openFile, word))
 		{
 			std::vector<char> stringVector(word.length());
 			std::copy(word.begin(), word.end(), stringVector.begin());
 			words.push_back(stringVector);
 			//pushing the vector to main deque
-			index = words.size();
-
-			if (!compare(stringVector.at(0), beforeChar))
-			{
-				wordsIndex.push_back(std::make_pair(tolower(stringVector.at(0)), index));
-			}
-			beforeChar = stringVector.at(0);
 		}
 	}
 	else
@@ -73,7 +63,7 @@ int main()
 	std::string input;
 	std::cout << "Enter word to find ------------" << std::endl;
 	std::cin >> input;
-	// input = "pn";  // Written Just for testing
+	// input = "somewhx"; // Written Just for testing
 
 	std::vector<char> inputVector(input.length());
 	std::copy(input.begin(), input.end(), inputVector.begin());
@@ -85,62 +75,72 @@ int main()
 		std::cout << "No matching words found " << std::endl;
 		std::cout << "Suggestions for you are \n";
 
-		char currentChar = inputVector.at(0);
-		std::vector<std::pair<char, int>>::iterator letterIterator = find_if(wordsIndex.begin(), wordsIndex.end(), [&currentChar](const std::pair<char, int> &element)
-																			 { return compare(element.first, currentChar); });
-		// just handling valid case only
-		if (letterIterator != wordsIndex.end())
+		std::deque<std::vector<char>> suggestions, previousSuggestion;
+		// words.size() is necessary because it won't copy if not given
+		suggestions = words;
+		// can just use equal operator;
+		suggestions.erase(std::remove_if(
+							  suggestions.begin(), suggestions.end(),
+							  [&](const std::vector<char> elem)
+							  {
+								  return elem.size() != inputVector.size(); // put your condition here
+							  }),
+						  suggestions.end());
+
+		for (int cc = 1; cc < inputVector.size(); cc++)
 		{
-			int currentLetterIndex = (*letterIterator).second;
-			int nextLetterIndex = (*(letterIterator + 1)).second;
-			std::deque<std::vector<char>> suggestions(words.size());
-			std::copy(words.begin(), words.end(), suggestions.begin());
-			suggestions.erase(std::remove_if(
-								  suggestions.begin(), suggestions.end(),
-								  [&](const std::vector<char> elem)
-								  {
-									  return elem.size() != inputVector.size(); // put your condition here
-								  }),
+			previousSuggestion = suggestions;
+			// for saving suggestions one step before
+
+			suggestions.erase(std::remove_if(suggestions.begin(), suggestions.end(),
+											 [&](const std::vector<char> elem)
+											 {
+												 return !compare(elem.at(cc), inputVector.at(cc));
+											 }),
 							  suggestions.end());
-
-			for (int cc = 1; cc < inputVector.size(); cc++)
+			if (suggestions.size() == 0)
 			{
-				std::deque<std::vector<char>> previousSuggestion = suggestions;
-
-				suggestions.erase(std::remove_if(suggestions.begin(), suggestions.end(),
-												 [&](const std::vector<char> elem)
-												 {
-													 return !compare(elem.at(cc), inputVector.at(cc));
-												 }),
-								  suggestions.end());
-
-				if (suggestions.size() == 0)
+				suggestions = previousSuggestion;
+				cc++;
+				if (cc < inputVector.size())
 				{
-					suggestions = previousSuggestion;
-					cc++;
-					if (cc < inputVector.size())
+					suggestions.erase(std::remove_if(suggestions.begin(), suggestions.end(),
+													 [&](const std::vector<char> elem)
+													 {
+														 //  std::cout << "elem =" << elem.at(cc) << "    input =" << inputVector.at(cc) << std::endl;
+														 return !compare(elem.at(cc), inputVector.at(cc));
+													 }),
+									  suggestions.end());
+					if (suggestions.size() == 0)
 					{
-						suggestions.erase(std::remove_if(suggestions.begin(), suggestions.end(),
-														 [&](const std::vector<char> elem)
-														 {
-															 //  std::cout << "elem =" << elem.at(cc) << "    input =" << inputVector.at(cc) << std::endl;
-															 return !compare(elem.at(cc), inputVector.at(cc));
-														 }),
-										  suggestions.end());
-						previousSuggestion = suggestions;
-					}
-					else
-					{
+						suggestions = previousSuggestion;
 						break;
 					}
+					previousSuggestion = suggestions;
+				}
+				else
+				{
+					break;
 				}
 			}
-			if (suggestions.size() > 5)
-			{
-				suggestions.erase(suggestions.begin() + 5, suggestions.end());
-			}
-			displayVectorDeque(suggestions);
 		}
+		if (suggestions.size() > 5)
+		{
+			previousSuggestion = suggestions;
+			suggestions.erase(std::remove_if(suggestions.begin(), suggestions.end(), [&](const std::vector<char> elem)
+											 { return !compare(elem.at(0), inputVector.at(0)); }),
+							  suggestions.end());
+
+			if (suggestions.size() == 0)
+				suggestions = previousSuggestion;
+			if (suggestions.size() > 5)
+				suggestions.erase(suggestions.begin() + 5, suggestions.end());
+		}
+		if (suggestions.size() == 0)
+		{
+			suggestions = previousSuggestion;
+		}
+		displayVectorDeque(suggestions);
 	}
 	else
 	{
@@ -149,8 +149,6 @@ int main()
 		displayCharVector(*(foundIterator - 1));
 		displayCharVector(*(foundIterator + 1));
 	}
-
 	return 0;
 }
-
 // speed changed from (5-6) to (6-8)
